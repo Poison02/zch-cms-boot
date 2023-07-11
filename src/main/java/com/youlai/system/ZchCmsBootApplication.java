c9b50ba2350a3e25ce2ee349601f9d0326e72027
zchhhhh <2069820192@qq.com>
2023-07-11 17:19:56 +0800

    fix: fix bugs

---
+++ src/main/java/com/youlai/system/ZchCmsBootApplication.java
@@ -0,0 +1,14 @@
+package com.youlai.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zch
+ */
+@SpringBootApplication
+public class ZchCmsBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZchCmsBootApplication.class, args);
+    }
+}
